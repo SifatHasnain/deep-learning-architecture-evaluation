ce30d2cec6a0a2d6f31b60b83a617ce27867f44c
sifat95 <sifathasnain95@gmail.com>
2022-06-03 15:56:32 +0600

    feat: add code structure

--- train.py
+++ train.py
@@ -1,17 +1,27 @@
+import os
+
 from tensorflow.keras import Model
 import tensorflow as tf
 from tensorflow.keras.callbacks import ReduceLROnPlateau
 
 import config
 import models
-from dataprep import generate_dataset
+from dataprep import generate_train_dataset
 
-def get_model():
-    model = models.Alexnet()
-    # if config.model == "resnet18":
-    #     model = resnet_18()
-    model.build(input_shape=(config.image_height, config.image_width, config.channels))
+def get_model(model_name, input_shape, num_channels, num_classes):
+    
+    if model_name == "cnn":
+        model = models.CNN()
+    elif model_name == "alexnet":
+        model = models.Alexnet(input_shape.image_height, input_shape.image_width)
+    elif model_name == "resnet":
+        model = models.Resnet()
+    else:
+        model = models.InceptionNet()
+
+    model.build(input_shape=(input_shape.image_height, input_shape.image_width, num_channels), num_classes=num_classes)
     model.summary()
+
     return model
 
 if __name__ == '__main__':
@@ -21,12 +31,22 @@ if __name__ == '__main__':
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
 
-# get the original_dataset
-    train_dataset, valid_dataset, test_dataset = generate_dataset()
+    # load configs and resolve paths
+    train_data_config = (config.train_dir, config.valid_dir, config.image_height, config.image_width, config.TRAIN_BATCH_SIZE, config.VALID_BATCH_SIZE)
+    result_save_path = os.path.join(config.result_dir, config.model)
+    if not os.path.exists(result_save_path):
+        os.mkdir(result_save_path)
+    log_dir = os.path.join(result_save_path, "logs_{}".format(config.version))
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
 
-# create model
-    model = get_model()
+    # get the original_dataset
+    train_dataset, valid_dataset = generate_train_dataset(train_data_config)
+
+    # create model
+    model = get_model(config.model, (config.image_height, config.image_width), config.num_channels, config.num_classes)
     
+    # set the callbacks
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
 
     rlrop = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10)
@@ -35,10 +55,14 @@ if __name__ == '__main__':
 
     # start training
     model.fit(train_dataset,
-                        epochs= config.EPOCHS,
-                        steps_per_epoch= train_dataset.samples // config.BATCH_SIZE,
-                        validation_data=valid_dataset,
-                        validation_steps= valid_dataset.samples // config.BATCH_SIZE,
-                        callbacks=callback_list,
-                        verbose=0)
-    model.save(config.model_dir)
+                epochs= config.EPOCHS,
+                steps_per_epoch= train_dataset.samples // config.BATCH_SIZE,
+                validation_data=valid_dataset,
+                validation_steps= valid_dataset.samples // config.BATCH_SIZE,
+                callbacks=callback_list,
+                verbose=1)
+
+    # save model
+    model_name="{}_{}_dogcat.h5".format(config.model, config.version)
+    model_save_path = os.path.join(result_save_path, model_name)
+    model.save(model_save_path)
---
+++ utils.py
@@ -0,0 +1,44 @@
+import os
+from sklearn.metrics import confusion_matrix
+import numpy as np
+
+import config
+from train import model
+from dataprep import generate_test_dataset 
+
+def evaluate():
+    
+    # get the test_dataset
+    test_data_config = (config.test_dir, config.image_height, config.image_width, config.TEST_BATCH_SIZE)
+    test_dataset = generate_test_dataset(test_data_config)
+        
+    x_test, label_batch  = next(iter(test_dataset))
+
+    # plot the images: each image is 227x227 pixels
+    # for i in range(8):
+    #     ax = fig.add_subplot(2, 4, i + 1, xticks=[], yticks=[])
+    #     ax.imshow(x_test[i,:],cmap=plt.cm.gray_r, interpolation='nearest')
+    
+    #     if prediction_values[i] == np.argmax(label_batch[i]):
+    #         # label the image with the blue text
+    #         ax.text(3, 17, config.class_names[prediction_values[i]], color='blue', fontsize=14)
+    #     else:
+    #         # label the image with the red text
+    #         ax.text(3, 17, config.class_names[prediction_values[i]], color='red', fontsize=14)
+
+    #confusion matrix
+    predict_x=model.predict(x_test) 
+
+    pred = np.round(predict_x)
+    pred = np.argmax(pred,axis=1)
+
+    label = np.argmax(label_batch,axis=1)
+
+    pred = pred.tolist()
+    label = label.tolist()
+
+    cf= confusion_matrix(pred,label)
+    
+    accuracy = model.evaluate(x_test,label_batch)
+
+    return pred, label, x_test, cf, accuracy
