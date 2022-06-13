069fa1940039a5ff5510ca1beec36b65c7628e7c
Jf.Raisa <40927500+raisa314@users.noreply.github.com>
2022-06-13 17:11:27 +0600

    Update models.py

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras import layers
 
 # AlexNet model
 class AlexNet(Model):
@@ -69,3 +70,40 @@ class AlexNet(Model):
         x = self.fc4(x)
     
         return x
+#cnn
+class Conv(layers.Layer):
+  def __init__(self, filters, kernel_size):
+        super(Conv, self).__init__(name='')
+
+        self.conv = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size)
+        self.bn = tf.keras.layers.BatchNormalization()
+        self.relu = tf.keras.layers.ReLU()
+        self.pool = tf.keras.layers.MaxPool2D(pool_size=(2, 2))
+        
+  def call(self, inputs, training=True):
+        x = self.conv(inputs)
+        x = self.bn(x, training=training)
+        x = self.relu(x)
+        x = self.pool(x)
+        return x
+class CNN(Model):
+  def __init__(self, input_shape, num_classes):
+    super(CNN, self).__init__(name='')
+    self.conv1 = Conv(filters=32, kernel_size=(3, 3))
+    self.conv2 = Conv(filters=64, kernel_size=(3, 3))
+    self.conv3 = Conv(filters=128, kernel_size=(3, 3))
+    self.conv4 = Conv(filters=128, kernel_size=(3, 3))
+    self.flatten = Flatten()
+    self.fc1 = Dense(512, activation= 'relu')
+    self.fc2 = Dense(2, activation= 'relu')
+  
+  def call(self, input_tensor, training=False):
+    x = self.conv1(input_tensor)
+    x = self.conv2(x)
+    x = self.conv3(x)
+    x = self.conv4(x)
+    x = self.flatten(x)
+    x = self.fc1(x)
+    x = self.fc2(x)
+
+    return x
